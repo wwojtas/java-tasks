3bb392e5d8a7f43bb2e1c6b5060e68c36ca9cbaf
wwojtas <wojciech.wus.programista@gmail.com>
2022-02-02 12:29:40 +0100

    Linked List Iterator

---
+++ src/Kolekcje/Linked_list_iterator_manipulation/MainLinkedListIteratorManipulation.java
@@ -0,0 +1,41 @@
+package Kolekcje.Linked_list_iterator_manipulation;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class MainLinkedListIteratorManipulation {
+    public static void main(String[] args) {
+
+
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("Adam");
+        String p1 = "Ola";
+        list.add(p1);
+        list.addFirst("Rafał");
+        list.addLast("Olaf");
+
+        ListIterator<String> iterator = list.listIterator();
+
+        while (iterator.hasNext()) {
+            String str = iterator.next();
+            if(str.equalsIgnoreCase("ola")) {
+                iterator.set("Zuza");
+            }
+            if(str.equalsIgnoreCase("Adam")) {
+                iterator.add("Karol");
+            }
+
+            if(str.equalsIgnoreCase("Rafał")) {
+                iterator.remove();
+            }
+
+        }
+        Iterator<String> iter = list.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+
+    }
+}
