c9f51e3f9f79a0d1883d0b98b5bf6e9aafd8c7ac
wwojtas <wojciech.wus.programista@gmail.com>
2022-02-06 16:35:15 +0100

    Watki Thread

---
+++ src/Threads/ThreadExtendExample/TestNewThread.java
@@ -0,0 +1,27 @@
+package Threads.ThreadExtendExample;
+
+public class TestNewThread extends Thread {
+
+    private int sleepTime;
+
+    public TestNewThread(String name, int sleepTime) {
+        super(name);
+        this.sleepTime = sleepTime;
+    }
+
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Wątek - " + this.getName() + " : " + i);
+            try {
+                sleep(this.sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+    }
+
+}
---
+++ src/Threads/ThreadExtendExample/ThreadExtendExample.java
@@ -0,0 +1,17 @@
+package Threads.ThreadExtendExample;
+
+public class ThreadExtendExample {
+    public static void main(String[] args) {
+
+        TestNewThread thread1 = new TestNewThread("thread1", 1000);
+        thread1.start();
+
+        TestNewThread thread2 = new TestNewThread("thread2", 700);
+        thread2.start();
+
+//        TestNewThread thread3 = new TestNewThread("thread3", 1100);
+//        thread3.start();
+
+
+    }
+}
