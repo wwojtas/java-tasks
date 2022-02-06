7e053c4048d94dda76c55eeea7753aa5d8dc6463
wwojtas <wojciech.wus.programista@gmail.com>
2022-02-06 21:26:43 +0100

    Deamon thread

---
+++ src/Threads/Deamon/MainDeamon.java
@@ -0,0 +1,28 @@
+package Threads.Deamon;
+
+public class MainDeamon {
+    public static void main(String[] args)
+            throws InterruptedException{
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                while (true){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Deamon thread: " + i);
+                    i++;
+                }
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+
+        Thread.sleep(5000);
+        System.out.println( "Quit main thread");
+    }
+}
