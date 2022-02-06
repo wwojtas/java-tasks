195b141088de224f684542ccd3e640924a9013c4
wwojtas <wojciech.wus.programista@gmail.com>
2022-02-06 21:42:48 +0100

    Clock in Swing

---
+++ src/Threads/SwingClock/MainSwingClock.java
@@ -0,0 +1,49 @@
+package Threads.SwingClock;
+
+import javax.swing.*;
+import java.time.LocalDateTime;
+
+
+public class MainSwingClock extends JFrame implements Runnable {
+
+    private Thread thread;
+    String hour, minutes, seconds;
+    JLabel label;
+
+    public MainSwingClock() {
+        label = new JLabel("", JLabel.CENTER);
+        label.setBounds(5,5,100,40);
+        add(label);
+        setSize(100,80);
+        setLayout(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try{
+            while (true){
+                LocalDateTime today = LocalDateTime.now();
+               hour = "" + today.getHour();
+               minutes = String.format("%02d", today.getMinute());
+               seconds = String.format("%02d", today.getSecond());
+               label.setText(hour + ":" + minutes + ":" + seconds );
+               thread.sleep(1000);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        MainSwingClock clock = new MainSwingClock();
+
+
+    }
+
+}
