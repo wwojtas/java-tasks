a3867cf1680af0de875279c3011acdaede9c3902
wwojtas <wojciech.wus.programista@gmail.com>
2022-02-08 20:06:40 +0100

    File class

--- src/Files/MainFile.java
+++ src/Files/MainFile.java
@@ -16,30 +16,49 @@ public class MainFile {
             System.out.println("Wynik utworzenia pliku: " + fileCreated);
         } else {
             System.out.println("Plik już istnieje");
+
+            // czy plik może być modyfikowany - czy można czytac plik?
+            System.out.println("Czy mozna czytać plik: " +file.canRead());
+
+            // czy mozna zapisać do pliku
+            System.out.println("czy mozna zapisać do pliku: " + file.canWrite());
+
+            System.out.println("Czy to zwykły plik: " + file.isFile());
+
+            System.out.println("Czy plik jest katalogiem: " + file.isDirectory());
+
+            System.out.println("Nazwa pliku: " + file.getName());
+
+            System.out.println("Katalog w ktorym jest plik: " + file.getParent());
+
+            System.out.println("Sciezka do pliku: " + file.getPath());
+            System.out.println("Sciezka absolutna do pliku: " + file.getAbsolutePath());
+
+            System.out.println("Plik ukryty: " + file.isHidden());
+
+            long lastModified = file.lastModified();
+            System.out.println("Modyfikowany: " + (new Date(lastModified)));
+
+            System.out.println("Wielkosć pliku: " + file.length());
         }
-        // czy plik może być modyfikowany - czy można czytac plik?
-        System.out.println("Czy mozna czytać plik: " +file.canRead());
 
-        // czy mozna zapisać do pliku
-        System.out.println("czy mozna zapisać do pliku: " + file.canWrite());
 
-        System.out.println("Czy to zwykły plik: " + file.isFile());
+        File filesDir = new File("files");
+        if( filesDir.exists() ) {
+            System.out.println("\n Lista plikow w katalogu files");
+            String files[] = filesDir.list();
+            for (String f : files) {
+                System.out.println(f);
+            }
+        }
 
-        System.out.println("Czy plik jest katalogiem: " + file.isDirectory());
+        // tworzenie katalogu
+        File dir = new File("files\\test_dir");
+        if( !dir.exists() ) {
+            dir.mkdir();
+        }
 
-        System.out.println("Nazwa pliku: " + file.getName());
 
-        System.out.println("Katalog w ktorym jest plik: " + file.getParent());
-
-        System.out.println("Sciezka do pliku: " + file.getPath());
-        System.out.println("Sciezka absolutna do pliku: " + file.getAbsolutePath());
-
-        System.out.println("Plik ukryty: " + file.isHidden());
-
-        long lastModified = file.lastModified();
-        System.out.println("Modyfikowany: " + (new Date(lastModified)));
-
-        System.out.println("Wielkosć pliku: " + file.length());
 
 
     }
