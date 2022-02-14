bc682f0b403327cebbcfc67765b13f60da43eb01
wwojtas <wojciech.wus.programista@gmail.com>
2022-02-14 22:11:17 +0100

    Typ generyczny

---
+++ src/ZMyOwnClass/TypGeneryczny/MainBoxForFigures.java
@@ -0,0 +1,9 @@
+package ZMyOwnClass.TypGeneryczny;
+
+public class MainBoxForFigures {
+    public static void main(String[] args) {
+
+        BoxForFigures<Circle> circleBox = new BoxForFigures<>(new Circle());
+
+    }
+}
