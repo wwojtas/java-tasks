1da655b34d00068304248457ef176cb317384518
wwojtas <wojciech.wus.programista@gmail.com>
2022-04-05 13:25:09 +0200

    JSON - odczyt pliku

---
+++ src/programowanieSieciowe/JSONFileParsingExample.java
@@ -0,0 +1,35 @@
+package programowanieSieciowe;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class JSONFileParsingExample {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(
+                new FileReader(
+                        new File("example.json")));
+        StringBuffer buf = new StringBuffer();
+        String str;
+        while ( (str = br.readLine()) != null) {
+            buf.append(str);
+        }
+
+        String jsonStr = buf.toString();
+        System.out.println(jsonStr);
+
+        JSONObject jsonObject = new JSONObject(jsonStr);
+        JSONObject company = jsonObject.getJSONObject("company");
+        System.out.println("Company name: " + company.getString("name"));
+
+        JSONArray arr = company.getJSONArray("products");
+        for (int i = 0; i < arr.length(); i++) {
+            System.out.println("product: " + arr.getString(i));
+        }
+
+    }
+}
